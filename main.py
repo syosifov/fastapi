4fc415ec32f0cbba86bef4cd051a34aab4455f2f
Strashimir Yosifov <syosifov@gmail.com>
2024-06-28 21:07:43 +0300

    socket2 p3.8 works

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from typing import Annotated
+from typing import Union
 from fastapi import (
     Cookie,
     Depends,
@@ -74,21 +74,21 @@ async def websocket_endpoint(websocket: WebSocket):
 
 async def get_cookie_or_token(
     websocket: WebSocket,
-    session: Annotated[str | None, Cookie()] = None,
-    token: Annotated[str | None, Query()] = None,
+    session: Union[str, None] = Cookie(default=None),
+    token: Union[str, None] = Query(default=None),
 ):
     if session is None and token is None:
         raise WebSocketException(code=status.WS_1008_POLICY_VIOLATION)
     return session or token
 
 
+
 @app.websocket("/items/{item_id}/ws")
 async def websocket_endpoint(
-    *,
     websocket: WebSocket,
     item_id: str,
-    q: int | None = None,
-    cookie_or_token: Annotated[str, Depends(get_cookie_or_token)],
+    q: Union[int, None] = None,
+    cookie_or_token: str = Depends(get_cookie_or_token),
 ):
     await websocket.accept()
     while True:
@@ -99,3 +99,4 @@ async def websocket_endpoint(
         if q is not None:
             await websocket.send_text(f"Query parameter q is: {q}")
         await websocket.send_text(f"Message text was: {data}, for item ID: {item_id}")
+
