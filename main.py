3e128f5da786af09af847ced7be4276fad400471
Strashimir Yosifov <syosifov@gmail.com>
2024-06-28 13:30:02 +0300

    var ws = new WebSocket("ws://fastapi-production-7c96.up.railway.app/ws");

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ html = """
         <ul id='messages'>
         </ul>
         <script>
-            var ws = new WebSocket("ws://localhost:8000/ws");
+            var ws = new WebSocket("ws://fastapi-production-7c96.up.railway.app/ws");
             ws.onmessage = function(event) {
                 var messages = document.getElementById('messages')
                 var message = document.createElement('li')
