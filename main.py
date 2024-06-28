eaf97422220312b29a1ceaeac79670bdad6571b2
Strashimir Yosifov <syosifov@gmail.com>
2024-06-28 12:03:21 +0300

    launch.json

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI # type: ignore
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+    return {"greeting": "Hello, World!!!", "message": "Welcome to FastAPI!"}
